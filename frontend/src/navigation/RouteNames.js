12149839271ef75fc0900b1185cea92d6b9ffcd9
Gregory Menezes <gregoryfontenele@gmail.com>
2018-10-23 14:31:06 -0300

    Add new screens, also logged and unlogged screen flux

--- frontend/src/navigation/RouteNames.js
+++ frontend/src/navigation/RouteNames.js
@@ -1,5 +1,10 @@
 export const RouteNames = {
   auth: 'AuthScreen',
   login: 'LoginScreen',
-  register: 'RegisterScreen'
+  register: 'RegisterScreen',
+  books: 'BooksScreen',
+  add_book: 'AddBookScreen',
+  add_author: 'AddAuthorScreen',
+  logged: 'Logged',
+  non_logged: 'NonLogged',
 };
--- frontend/src/navigation/Router.js
+++ frontend/src/navigation/Router.js
@@ -1,19 +1,43 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import { RouteNames } from './RouteNames';
 import AuthScreen from '../screens/AuthScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import BooksScreen from '../screens/book/BooksScreen';
+import AddBookScreen from '../screens/book/AddBookScreen';
+import AddAuthorScreen from '../screens/AddAuthorScreen';
 
-export const NonLoggedAppRouter = createStackNavigator(
+const LoggedAppRouter = createStackNavigator(
+  {
+    [RouteNames.books]: { screen: BooksScreen },
+    [RouteNames.add_book]: { screen: AddBookScreen },
+    [RouteNames.add_author]: { screen: AddAuthorScreen },
+  },
+  {
+    initialRouteName: RouteNames.books,
+    navigationOptions: { header: null },
+  },
+);
+
+const NonLoggedAppRouter = createStackNavigator(
   {
     [RouteNames.auth]: { screen: AuthScreen },
     [RouteNames.login]: { screen: LoginScreen },
     [RouteNames.register]: { screen: RegisterScreen },
   },
   {
-    initialRouteName: 'AuthScreen',
-    navigationOptions: {
-      header: null,
-    },
+    initialRouteName: RouteNames.auth,
+    navigationOptions: { header: null },
   },
 );
+
+export const createRootNavigator = (token) =>
+  createSwitchNavigator(
+    {
+      [RouteNames.logged]: LoggedAppRouter,
+      [RouteNames.non_logged]: NonLoggedAppRouter,
+    },
+    {
+      initialRouteName: token ? RouteNames.logged : RouteNames.non_logged,
+    },
+  );
